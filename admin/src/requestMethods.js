7ea1d88b26363532939b1e4b341f216794cce958
Yashasvi <98901285+Yashasvi-VII@users.noreply.github.com>
2022-04-06 21:18:28 +0530

    Update requestMethods.js

--- admin/src/requestMethods.js
+++ admin/src/requestMethods.js
@@ -1,10 +1,7 @@
 import axios from "axios";
-const BASEURL = "http://localhost:4000/api/";
-const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user)
-  .currentUser.accessToken;
-//const TOKEN =
-// "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxZmViMWJlYzkwNjA4ZWJkYjllZjFiZCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0NDYwMDkyOCwiZXhwIjoxNjQ0ODYwMTI4fQ.wgir-1FY2veuKZSJ9t27cuAKf_sExJxs3FR9oZteiMQ";
-//   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxZmViMWJlYzkwNjA4ZWJkYjllZjFiZCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0NDkyODAzMywiZXhwIjoxNjQ1MTg3MjMzfQ.uiFcRQZogKC2v9WJYuIEbCQ6spRI73OL85zIJp47egw";
+const BASEURL = provide backend baseurl
+const TOKEN =provide token here
+
 console.log(
   JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser
     .accessToken
