f613f50e143346ec2ce5af226031c8f612954e02
Yashasvi <98901285+Yashasvi-VII@users.noreply.github.com>
2022-04-06 21:27:18 +0530

    Update Feature.js

--- admin/src/components/Feature/Feature.js
+++ admin/src/components/Feature/Feature.js
@@ -80,38 +80,7 @@ function Feature({ type }) {
 
   return (
     <div className="featured">
-      {/* <div className="featuredItem">
-        <span className="featuredTitle">Revanue</span>
-        <div className="featuredMoneyContainer">
-          <span className="featuredMoney">$383</span>
-          <span className="featuredMoneyRate">
-            -1020
-            <ArrowDownward className="featuredIcon negative" />
-            <ArrowUpward className="featuredIcon" />
-          </span>
-        </div>
-        <span className="featuredSub">Compared to last month</span>
-      </div>
-      <div className="featuredItem">
-        <span className="featuredTitle">Sales</span>
-        <div className="featuredMoneyContainer">
-          <span className="featuredMoney">$4,415</span>
-          <span className="featuredMoneyRate">
-            -1.4 <ArrowDownward className="featuredIcon negative" />
-          </span>
-        </div>
-        <span className="featuredSub">Compared to last month</span>
-      </div>
-      <div className="featuredItem">
-        <span className="featuredTitle">Cost</span>
-        <div className="featuredMoneyContainer">
-          <span className="featuredMoney">$2,225</span>
-          <span className="featuredMoneyRate">
-            +2.4 <ArrowUpward className="featuredIcon" />
-          </span>
-        </div>
-        <span className="featuredSub">Compared to last month</span>
-      </div> */}
+   
       <div className="left">
         <span className="title">{data.title}</span>
         <span className="counter">
